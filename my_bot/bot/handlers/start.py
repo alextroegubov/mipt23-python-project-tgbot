7ef358f775e63470b902670add8a366b64eb2b83
troegubov <troegubov@wireless.iitp.ru>
2023-04-29 21:25:01 +0300

    add smiles to start

--- my_bot/bot/handlers/start.py
+++ my_bot/bot/handlers/start.py
@@ -1,10 +1,24 @@
+from bot.models import User
 from bot.main_bot import bot
+from telebot import types
 
-def act_on_start_command(message):
-	bot.send_message(
-		message.from_user.id, 
-	    "Hi, I am a telegram bot! I can help you study English words.\
-        Please register to continue. /reg")
-	
+def act_on_start_command(message: types.Message):
+
+	if User.objects.filter(external_id=message.from_user.id).exists():
+		user = User.objects.get(external_id=message.from_user.id)
+		bot.send_message(
+            message.from_user.id,
+            f"Hi, <b>{user.username}</b>✌️ Let's continue learning English 🧠",
+            parse_mode='HTML'
+        )
+	else:
+		bot.send_message(
+			message.from_user.id, 
+			(f"Hi, <b>{message.from_user.username}</b>✌️, I am a telegram bot🤖\n"
+    		  "I can help you study English words🤓 "
+			  "Please register to continue. /reg"),
+			parse_mode='HTML'
+		)
+
 def register_handler_start():
 	bot.register_message_handler(callback=act_on_start_command, commands=['start'])
