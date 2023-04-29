75252318305b4b0a8a4389e664e8cf22cd8b875a
troegubov <troegubov@wireless.iitp.ru>
2023-04-29 21:32:03 +0300

    refactoring reg.py

--- my_bot/bot/handlers/reg.py
+++ my_bot/bot/handlers/reg.py
@@ -1,14 +1,18 @@
 from bot.main_bot import bot
 from bot.models import User
+from telebot import types
 
-def act_on_reg_command(message):
+
+def act_on_reg_command(message: types.Message) -> None:
+    """ Primary handler to /reg command"""
 
     if User.objects.filter(external_id=message.from_user.id).exists():
         user = User.objects.get(external_id=message.from_user.id)
 
         bot.send_message(
             message.from_user.id,
-            f"<b>{user.username}</b>, you are already registered😅 To delete your profile, use /del",
+            (f"<b>{user.username}</b>, you are already registered😅"
+             "To delete your profile, use /del"),
             parse_mode='HTML'
         )
     else:
@@ -18,7 +22,9 @@ def act_on_reg_command(message):
         )
         bot.register_next_step_handler(message, callback=get_user_name)
 
-def get_user_name(message):
+
+def get_user_name(message: types.Message) -> None:
+    """ Handler to get user name """
     name = message.text
     new_user = User(username=name, external_id=message.from_user.id)
     new_user.save()
@@ -26,8 +32,10 @@ def get_user_name(message):
     bot.reply_to(
         message.from_user.id,
         (f"Successfully registered user <i>{name}</i>"
-          "Now you can use all functions of the bot. Use \help for more details.")
+         "Now you can use all functions of the bot. Use \help for more details.")
     )
 
-def register_handler_reg():
+
+def register_handler_reg() -> None:
+    """ Register handler for /reg command """
     bot.register_message_handler(commands=['reg'], callback=act_on_reg_command)
