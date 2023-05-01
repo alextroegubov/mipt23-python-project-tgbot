10478709d3eadf32e210f61e47d1f388d692ff83
troegubov <troegubov@wireless.iitp.ru>
2023-05-01 12:49:09 +0300

    ru interface + refactoring

--- my_bot/bot/handlers/addlesson.py
+++ my_bot/bot/handlers/addlesson.py
@@ -1,151 +1,166 @@
+from telebot import types
+from typing import Dict
+
 from bot.main_bot import bot
 from bot.models import User, LessonRecord
-
-from telebot import types
-from telebot.types import InlineKeyboardButton
-
-from typing import Dict
 
 from bot.utils import date_validator, date_str_to_django, date_django_to_str
 from bot.utils import get_yes_no_inline_keyboard
 
+# prefixes for queries handlers
 comment_prefix = 'comment_addlesson_inline_keyboard_'
 confirm_prefix = 'confirm_addlesson_inline_keyabord_'
 
+# to store temporary data before saving in db
 g_input_user_data: Dict[int, LessonRecord] = {}
 
-def act_on_addlesson_command(u_id):
 
-    if not User.objects.filter(external_id=u_id):
-        text = "Please, register first. /reg"
-        bot.send_message(u_id, text=text)
-        return
+def act_on_addlesson_command(u_id: int) -> None:
+    """Handler for addlesson command"""
 
     user = User.objects.get(external_id=u_id)
-    text = "Let's record new lesson. Enter the date of the lesson (dd.mm.yyyy):"
+    text = "Ура, записываем новый урок 🤠 Введите дату урока в формате дд.мм.гггг:"
     msg = bot.send_message(u_id, text=text)
 
     global g_input_user_data
     g_input_user_data[u_id] = LessonRecord(user=user)
 
-    bot.register_next_step_handler(
-        msg, 
-        callback=get_date
-    )
+    bot.register_next_step_handler(msg, callback=get_date)
 
-def get_date(message: types.Message):
 
-    u_id = message.from_user.id
+def get_date(message: types.Message) -> None:
+    """ Get date from the message"""
     global g_input_user_data
-    assert u_id in g_input_user_data
+    u_id = message.from_user.id
+
+    if not (u_id in g_input_user_data):
+        return
 
     entered_date = message.text
 
     # validation of entered data
     if not date_validator(entered_date):
-        text = f"Wrong data format: {entered_date}. Should be dd.mm.yyyy. Try again:"
+        text = (f"Неправильный формат даты (<b>{entered_date}</b>) 😓"
+                "Должно быть <u>дд.мм.гггг</u>"
+                "Давайте еще раз:")
 
-        bot.send_message(u_id, text=text)
-        bot.register_next_step_handler(
-            message, 
-            callback=get_date
-        )
+        bot.send_message(u_id, text=text, parse_mode='HTML')
+        bot.register_next_step_handler(message, callback=get_date)
         return
 
-    text = f"Fine, the date is {entered_date}. Enter duration in minutes:"
-    bot.send_message(u_id, text=text)
+    text = f"Записал <i>{entered_date}</i> 👌\nВведите длительность урока в минутах:"
+    bot.send_message(u_id, text=text, parse_mode='HTML')
+
     g_input_user_data[u_id].date = date_str_to_django(entered_date)
 
-    bot.register_next_step_handler(
-        message,
-        callback=get_duration
-    )
+    bot.register_next_step_handler(message, callback=get_duration)
 
 
-def get_duration(message: types.Message):
+def get_duration(message: types.Message) -> None:
+    """ Get duration from the message"""
     u_id = message.from_user.id
     global g_input_user_data
-    assert u_id in g_input_user_data
+
+    if not (u_id in g_input_user_data):
+        return
+
     g_input_user_data[u_id].duration = message.text
 
-    yes_text = 'Yes, of course'
-    no_text = 'No, thanks'
+    yes_text = 'Ну разумеется 😉'
+    no_text = 'Неа 🙄'
     kb = get_yes_no_inline_keyboard(comment_prefix, yes_text, no_text)
 
-    text = f"Fine, the duration is {message.text} minutes. Any comments?"
-
+    text = f"Отлично, записал <i>{message.text}</i> (минуты) 👌 Добавим пояснение?"
     bot.send_message(u_id, text=text, reply_markup=kb, parse_mode='HTML')
 
+
 def callback_on_comment(call: types.CallbackQuery) -> None:
+    """ Callback on question about comment """
     assert call.data.startswith(comment_prefix)
+    global g_input_user_data
 
     u_id = call.message.chat.id
     answer = call.data[len(comment_prefix):]
 
+    if not (u_id in g_input_user_data):
+        return
+
     if answer == 'yes':
-        msg = bot.send_message(u_id, text="Enter your comment then:")
-        bot.register_next_step_handler(
-            msg,
-            callback=get_lesson_record_comment
-        )
+        msg = bot.send_message(u_id, text="Тогда вводите пояснение 😂")
+        bot.register_next_step_handler(msg, callback=get_lesson_record_comment)
 
     elif answer == 'no':
-        msg = bot.send_message(u_id, text="Ok")
-        confirm_add_lesson(u_id)
+        g_input_user_data[u_id].comment = ''
+        msg = bot.send_message(u_id, text="Ну ладно...")
 
+        confirm_add_lesson(u_id)
     else:
         bot.send_message(u_id, text='smth wrong')
 
-def get_lesson_record_comment(message: types.Message):
-    u_id = message.from_user.id
 
+def get_lesson_record_comment(message: types.Message) -> None:
+    """ Get comment from the message """
+    u_id = message.from_user.id
     global g_input_user_data
-    assert u_id in g_input_user_data
+
+    if not (u_id in g_input_user_data):
+        return
+
     g_input_user_data[u_id].comment = message.text
     confirm_add_lesson(u_id)
 
-def confirm_add_lesson(u_id: int) -> None:
 
-    yes_text = "Yes, that's right"
-    no_text = "No, that's wrong"
+def confirm_add_lesson(u_id: int) -> None:
+    """ Show confirm keyboard"""
+    global g_input_user_data
+
+    if not (u_id in g_input_user_data):
+        return
+
+    lesson = g_input_user_data[u_id]
+    comment = f"\n({lesson.comment})" if lesson.comment != '' else ''
+    text = (f"Все правильно?\n"
+            f"Дата: <i>{date_django_to_str(lesson.date)}</i>\n"
+            f"Длительность: <i>{lesson.duration}</i>" +
+            comment)
+
+    yes_text = "Да, все так 👍"
+    no_text = "Я вводил другое 👎"
     kb = get_yes_no_inline_keyboard(confirm_prefix, yes_text, no_text)
 
-    global g_input_user_data
-    assert u_id in g_input_user_data
-    lesson = g_input_user_data[u_id]
-
-    text = ("Got it. Is everything alright? "
-           f"{lesson.duration} minutes lesson at {date_django_to_str(lesson.date)}"
-           f"({lesson.comment})")
-
-    bot.send_message(u_id, text=text, reply_markup=kb)
+    bot.send_message(u_id, text=text, reply_markup=kb, parse_mode='HTML')
 
 
-def callback_on_cofirm_add_lesson(call: types.CallbackQuery):
+def callback_on_cofirm_add_lesson(call: types.CallbackQuery) -> None:
+    """ Callback on confirmation"""
     assert call.data.startswith(confirm_prefix)
+    global g_input_user_data
 
     u_id = call.message.chat.id
     answer = call.data[len(confirm_prefix):]
 
-    global g_input_user_data
-    assert u_id in g_input_user_data
+    if not (u_id in g_input_user_data):
+        return
 
     if answer == 'yes':
         g_input_user_data[u_id].save()
-        text = f"Great! Lesson is added."
-
+        text = f"Супер! Урок успешно записан 🤝"
     elif answer == 'no':
-        text = f"Sorry... Let's try again. /addlesson"
+        text = f"Упс... Давайте попробуем еще раз 👉👈"
 
     # remove tmp input data
     g_input_user_data.pop(u_id)
 
-    bot.send_message(
-        u_id, 
-        text=text
-    )
+    bot.send_message(u_id, text=text, parse_mode='HTML')
 
-def register_handler_addlesson():
-    bot.register_message_handler(commands=['addlesson'], callback=act_on_addlesson_command)
-    bot.register_callback_query_handler(callback=callback_on_cofirm_add_lesson, func=lambda call: call.data.startswith(confirm_prefix))
-    bot.register_callback_query_handler(callback=callback_on_comment, func=lambda call: call.data.startswith(comment_prefix))
+
+def register_handler_addlesson() -> None:
+    """ Register handlers for addlesson command"""
+    bot.register_callback_query_handler(
+        callback=callback_on_cofirm_add_lesson,
+        func=lambda call: call.data.startswith(confirm_prefix)
+    )
+    bot.register_callback_query_handler(
+        callback=callback_on_comment,
+        func=lambda call: call.data.startswith(comment_prefix)
+    )
--- my_bot/bot/handlers/addword.py
+++ my_bot/bot/handlers/addword.py
@@ -1,136 +1,156 @@
+from telebot import types
+from typing import Dict
+
 from bot.main_bot import bot
 from bot.models import User, WordRecord
 from bot.utils import get_yes_no_inline_keyboard
 
-from telebot import types
-from typing import Dict
-
+# prefixes to distinguish between callback queris
 comment_prefix = 'comment_addword_inline_keyboard_'
 confirm_prefix = 'confirm_addword_inline_keyabord_'
 
+# to store data before saving in db
 g_input_user_data: Dict[int, WordRecord] = {}
 
-def act_on_addword_command(u_id) -> None:
-    """ Primary handler for /addword command"""
 
-    if not User.objects.filter(external_id=u_id):
-        text = "Please, register first. /reg"
-        bot.send_message(u_id, text=text)
-        return
+def act_on_addword_command(u_id: int) -> None:
+    """ Handler to addword command"""
 
     user = User.objects.get(external_id=u_id)
 
-    text = "Let's fill your dictionary🧐 Enter new word:"
+    text = "Ура, пополняем словарь😃 Введите новое слово:"
     msg = bot.send_message(u_id, text=text)
 
     global g_input_user_data
     g_input_user_data[u_id] = WordRecord(user=user)
 
     bot.register_next_step_handler(
-        msg, 
+        msg,
         callback=get_word_record_en_word
     )
 
-def get_word_record_en_word(message: types.Message) -> None:
-    u_id = message.from_user.id
 
+def get_word_record_en_word(message: types.Message) -> None:
+    """ Get enlish word from message"""
+    u_id = message.from_user.id
     global g_input_user_data
-    assert u_id in g_input_user_data
+
+    if not (u_id in g_input_user_data):
+        return
+
     g_input_user_data[u_id].en_word = message.text
 
-    text = f"Fine, <i>{message.text}</i> is writen👌 Enter translation:"
+    text = f"Записал <i>{message.text}</i>👌 Введите перевод:"
 
     bot.send_message(u_id, text=text, parse_mode='HTML')
 
     bot.register_next_step_handler(
-        message, 
+        message,
         callback=get_word_record_ru_translation
     )
 
-def get_word_record_ru_translation(message: types.Message) -> None:
-    u_id = message.from_user.id
 
+def get_word_record_ru_translation(message: types.Message) -> None:
+    """ Get translation from message """
+    u_id = message.from_user.id
     global g_input_user_data
-    assert u_id in g_input_user_data
+
+    if not (u_id in g_input_user_data):
+        return
+
     g_input_user_data[u_id].ru_translation = message.text
 
-    yes_text = 'Yes, of course'
-    no_text = 'No, thanks'
+    yes_text = 'Ну разумеется 😉'
+    no_text = 'Неа 🙄'
     kb = get_yes_no_inline_keyboard(comment_prefix, yes_text, no_text)
 
-    text = f"Fine, <i>{message.text}</i> is tranlation. Any additional comments?"
+    text = f"Перевод записан <i>{message.text}</i>👌 Добавим пояснение?"
 
     bot.send_message(u_id, text=text, reply_markup=kb, parse_mode='HTML')
 
+
 def callback_on_comment(call: types.CallbackQuery) -> None:
+    """ Callback on question about comment"""
     assert call.data.startswith(comment_prefix)
 
     u_id = call.message.chat.id
     answer = call.data[len(comment_prefix):]
 
     if answer == 'yes':
-        msg = bot.send_message(u_id, text="Enter your comment then:")
-        bot.register_next_step_handler(
-            msg,
-            callback=get_word_record_comment
-        )
-
+        msg = bot.send_message(u_id, text="Тогда вводите пояснение 😂")
+        bot.register_next_step_handler(msg, callback=get_word_record_comment)
     elif answer == 'no':
-        msg = bot.send_message(u_id, text="Ok")
+        msg = bot.send_message(u_id, text="Ну ладно...")
         confirm_add_word(u_id)
-
     else:
         bot.send_message(u_id, text='smth wrong')
 
-def get_word_record_comment(message: types.Message) -> None:
-    u_id = message.from_user.id
 
+def get_word_record_comment(message: types.Message) -> None:
+    """ Get comment from the message"""
+    u_id = message.from_user.id
     global g_input_user_data
-    assert u_id in g_input_user_data
+
+    if not (u_id in g_input_user_data):
+        return
+
     g_input_user_data[u_id].comment = message.text
+
     confirm_add_word(u_id)
 
 
 def confirm_add_word(u_id: int) -> None:
-
-    yes_text = "Yes, that's right"
-    no_text = "No, that's wrong"
-    kb = get_yes_no_inline_keyboard(confirm_prefix, yes_text, no_text)
-
+    """ Show confirm keyboard"""
     global g_input_user_data
-    assert u_id in g_input_user_data
+    if not (u_id in g_input_user_data):
+        return
+
     word = g_input_user_data[u_id]
 
-    text = f"Got it. Is everything alright?\
-           {word.en_word} = {word.ru_translation} ({word.comment})"
+    comment = f"\n({word.comment})" if word.comment != '' else ''
+    text = (f"Все правильно?\n"
+            f"Слово: <i>{word.en_word}</i>\n"
+            f"Перевод: <i>{word.ru_translation}</i>" +
+            comment)
 
-    bot.send_message(u_id, text=text, reply_markup=kb)
+    yes_text = "Да, все так 👍"
+    no_text = "Я вводил другое 👎"
+    kb = get_yes_no_inline_keyboard(confirm_prefix, yes_text, no_text)
+
+    bot.send_message(u_id, text=text, reply_markup=kb, parse_mode='HTML')
 
 
 def callback_on_cofirm_add_word(call: types.CallbackQuery):
+    """ Callback on confirmation question"""
     assert call.data.startswith(confirm_prefix)
+    global g_input_user_data
 
     u_id = call.message.chat.id
     answer = call.data[len(confirm_prefix):]
 
-    global g_input_user_data
-    assert u_id in g_input_user_data
+    if not (u_id in g_input_user_data):
+        return
 
     if answer == 'yes':
         g_input_user_data[u_id].save()
-        text = f"Great! {g_input_user_data[u_id].en_word} is added to your dictionary."
+        text = f"Супер! <i>{g_input_user_data[u_id].en_word}</i> успешно добавлено в словарь 🤝"
 
     elif answer == 'no':
-        text = f"Sorry... Let's try again. /addword"
+        text = f"Упс... Давайте попробуем еще раз 👉👈"
 
     # remove tmp input data
     g_input_user_data.pop(u_id)
 
-    bot.send_message(
-        u_id, 
-        text=text
-    )
+    bot.send_message(u_id, text=text, parse_mode='HTML')
 
-def register_handler_addword():
-    bot.register_callback_query_handler(callback=callback_on_cofirm_add_word, func=lambda call: call.data.startswith(confirm_prefix))
-    bot.register_callback_query_handler(callback=callback_on_comment, func=lambda call: call.data.startswith(comment_prefix))
+
+def register_handler_addword() -> None:
+    """ register handlers for addword command"""
+    bot.register_callback_query_handler(
+        callback=callback_on_cofirm_add_word,
+        func=lambda call: call.data.startswith(confirm_prefix)
+    )
+    bot.register_callback_query_handler(
+        callback=callback_on_comment,
+        func=lambda call: call.data.startswith(comment_prefix)
+    )
